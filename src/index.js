7a8664d765f84abb90cc9151b0f0bbc4759581ca
PavelIvanov4 <94687008+PavelIvanov4@users.noreply.github.com>
2022-01-20 00:18:11 +0300

    feat : my next try!

--- src/index.js
+++ src/index.js
@@ -1,94 +1,26 @@
+let firstArray = ["one", "two", "three", "four", "five", "six", "sevent", "eigth", "nine", "ten"];
+let secondArray = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+
 function helpReadable (number) {
   let num = parseInt(number);
   let out = "";
-  switch (parseInt(num / 100)%10) {
-    case 0 : break; 
-    case 1 : out += "one hundred ";
-    break;
-    case 2 : out += "two hundred "; 
-    break;
-    case 3 : out += "three hundred ";
-    break;
-    case 4 : out += "four hundred ";
-    break;
-    case 5 : out += "five hundred ";  
-    break;
-    case 6 : out += "six hundred ";
-    break;
-    case 7 : out += "seven hundred ";
-    break;
-    case 8 : out += "eight hundred ";
-    break;
-    case 9 : out += "nine hundred ";
-    break;
+  let result1 = parseInt(num/100) % 10;
+  
+  if(result1 != 0)
+    out += firstArray[result1 - 1];
+  
+  let tens = parseInt(num / 10) % 10;
+  let ones = num % 10;
+  
+  if(tens != 0){
+    if(ones == 1)
+      out += secondArray[ones - 1];
+    else
+      out += secondArray[ones - 1];
   }
-    let tens = parseInt(num / 10)%10;
-    let ones = num%10;
-    switch (tens) {
-    case 0 : break; 
-    case 1 : switch (ones) {
-    case 0 : out += "ten";
-    break; 
-    case 1 : out += "eleven";
-    break;
-    case 2 : out += "twelve"; 
-    break;
-    case 3 : out += "thirteen";
-    break;
-    case 4 : out += "fourteen";
-    break;
-    case 5 : out += "fifteen";  
-    break;
-    case 6 : out += "sixteen";
-    break;
-    case 7 : out += "seventeen";
-    break;
-    case 8 : out += "eighteen";
-    break;
-    case 9 : out += "nineteen";
-    break;
-    }
-    break;
-    case 2 : out += "twenty "; 
-    break;
-    case 3 : out += "thirty ";
-    break;
-    case 4 : out += "forty ";
-    break;
-    case 5 : out += "fifty ";  
-    break;
-    case 6 : out += "sixty ";
-    break;
-    case 7 : out += "seventy ";
-    break;
-    case 8 : out += "eighty ";
-    break;
-    case 9 : out += "ninety ";
-    break;
-    }
-    if(tens != 1) {
-      switch (ones) {
-    case 0 : break;
-    case 1 : out += "one";
-    break;
-    case 2 : out += "two"; 
-    break;
-    case 3 : out += "three";
-    break;
-    case 4 : out += "four";
-    break;
-    case 5 : out += "five";  
-    break;
-    case 6 : out += "six";
-    break;
-    case 7 : out += "seven";
-    break;
-    case 8 : out += "eight";
-    break;
-    case 9 : out += "nine";
-    break;
-    }
-    }
+  
+  if(tens != 1 && ones != 0)
+    out += firstArray[ones - 1];
   
   return out;
 }
@@ -97,15 +29,19 @@ module.exports = function toReadable (number) {
   if(number == 0) {
     return "zero";
   }
+  
   let str1 = number.toString(10);
+  
   if(str1.length < 4) {
     return helpReadable(number);
-   } else if(str1.length < 7) {
+   } 
+  else if(str1.length < 7) {
      let out = helpReadable(number / 1000) + " thousand " + helpReadable(number%1000);
      return out;
-   } else {
+  } 
+  else {
      let out = helpReadable(number / 1000000) + " million " + helpReadable(number/1000) ; if(helpReadable(number / 1000) != "") {out += ' thousand ' ;}
      out += helpReadable(number%1000);
      return out;
    }
-  }
+}
